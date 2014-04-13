098783faed5e285a873dae666e345b21a942ef2f
nkotzev <nkotzev@uw.edu>
2014-04-12 22:22:53 -0700

    Initial source code

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,35 @@
+// Calculator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to the Calulator App\n";
+	int x;
+	int y;
+	char z;
+
+	int quit = 0;
+	while (!quit)
+	{
+		cout << "*";
+		cout << "Enter a integer: ";
+		cin >> x;
+		cout << " Enter another integer: ";
+		cin >> y;
+		cout << "Enter a charecter";
+		cin >> z; 
+
+		if (z == '+')
+		{
+			int result = x + y;
+			cout << result;
+		}
+	}
+	return 0;
+}
+
